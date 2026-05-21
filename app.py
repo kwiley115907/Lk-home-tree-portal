58813af942ac89105a5f90623167b010e91be846
Wileyk <wiley11590706@gmail.com>
2026-05-21 01:24:59 -0500

    Add legal and contact pages

--- app.py
+++ app.py
@@ -579,6 +579,23 @@ def sign_invoice(invoice_id: int):
     return render_template("signature.html", invoice=invoice)
 
 
+
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
+
+@app.route("/terms")
+def terms():
+    return render_template("terms.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run()
 
