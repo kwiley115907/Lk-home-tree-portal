0434e036876c50b983fa1b088e3a12b1c313e83a
Wileyk <wiley11590706@gmail.com>
2026-06-02 17:58:38 -0500

    Use pg8000 for Render PostgreSQL

--- config.py
+++ config.py
@@ -7,9 +7,18 @@ class Config:
     database_url = os.environ["DATABASE_URL"]
 
     if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+        database_url = database_url.replace(
+            "postgres://",
+            "postgresql+pg8000://",
+            1,
+        )
+
+    elif database_url.startswith("postgresql://"):
+        database_url = database_url.replace(
+            "postgresql://",
+            "postgresql+pg8000://",
+            1,
+        )
 
     SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
